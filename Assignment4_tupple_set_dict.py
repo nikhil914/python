e5e3ed1ebdd4100f9dc5051ba2d5ac92b260d144
rahul6316057 <rt56277@gmail.com>
2018-06-14 17:54:26 +0530

    assignments are updated

---
+++ Assignment4_tupple_set_dict.py
@@ -0,0 +1,64 @@
+print("question no 1 & 2")
+t=(1,3,5,7,9,11,)
+t1=('india','china','japan')
+print(t)
+print(len(t))		#find the length
+print(max(t)) 		#find maximum
+print(min(t))		#find minimum
+print(t1)
+print(len(t1))
+print(max(t1))
+print(min(t1))
+print("\n\n\n")
+
+print("question no 3 ")
+t=(1,3,5,7,9,11)
+q=1
+for a in range(0,6):
+	k=t[a]					
+	q=k*q
+print(q)
+print("\n\n\n")
+
+print("question no 4(1,2,3) ")
+l=[0,0,0]
+l[0]=input("enter 1st element of 1st set ")
+l[1]=input("enter 2nd element of 1st set ")
+l[2]=input("enter 3rd element of 1st set ")
+
+l1=[0,0,0]
+l1[0]=input("enter 1st element of 2nd set ")
+l1[1]=input("enter 2nd element of 2nd set ")
+l1[2]=input("enter 3rd element of 2nd set ")
+s1=set(l)			#type casting
+s2=set(l1)
+print(s1)
+print(s2)
+print(s1-s2) 		#difference of two sets
+print(s1&s2)
+print("\n\n\n")
+
+print("question no 5 & 6 ")
+d={}
+l=[]
+for a in range(0,10):
+	name=input("enter a name ")
+	marks=int(input("enter any marks "))
+	l.append(marks)
+	d[name]=marks
+print(d)
+l.sort()				#sort the list
+print("the sorted marks are",l)
+print("\n\n\n")
+
+print("question no 7 ")
+s='MISSISSIPPI'
+m=list(s)
+a=m.count('M')		#count the m in mississippi
+b=m.count('I')
+c=m.count('S')
+d=m.count('P')
+g={'M':a,'I':b,'S':c,'P':d}
+print("the dictionary of MISSISSIPPI of each letter is",g)
+print("\n\n\n")
+
---
+++ list.py
@@ -0,0 +1,95 @@
+print("question no 1 method 1")
+l=[0,0,0]
+l[0]=input("enter 1st element of list ")
+l[1]=input("enter 2nd element of list ")
+l[2]=input("enter 3rd element of list ")
+print(l)
+print("\n\n\n")
+
+
+#method 2 
+print("question no 1 method 2")
+x=int(input('enter 1st element of list '))
+y=int(input('enter 2nd element of list '))
+z=int(input('enter 3rd element of list '))
+l=[x,y,z]
+print(l)
+print("\n\n\n")
+
+
+
+print("question no 2")
+x=['india','japan','chaina']
+x1=['google','apple','facebook','microsoft','tesle']
+x.extend(x1)
+print(x)
+print("\n\n\n")
+
+print("question no 3")
+y=[1,2,1,4,2,3,2,2,3,2]
+print("the list is"y)
+print("the no of 2 in above list ",y.count(2))
+print("\n\n\n")
+
+print("question no 4")
+z=[2,4,1,8,5,6]
+z.sort()         #sort the list ("ascending order")
+print(z)
+print("\n\n\n")
+
+print("question no 5")
+a=[1,2,7,5,3]
+b=[5,2,1,4,6]
+c=a+b
+print("merge list is",c)
+c.sort()					#sort the list ("ascending order")
+print("sorted list is",c)
+print("\n\n\n")
+
+print("question no 6")
+p=[]
+print('stack is',p)
+p.append(int(input("enter any no to push in stack ")))   #for adding a new element in list(stack)
+p.append(int(input("enter any no to push in stack ")))
+p.append(int(input("enter any no to push in stack ")))
+print(p)
+p.pop()                                    #for removing a element from list(stack)
+print('after pop the stack is',p)
+print("\n\n\n")
+
+p=[]
+print('queue is',p)
+p.insert(0,int(input("enter any no to push in queue ")))    #for adding a new element in list(queue)
+p.insert(0,int(input("enter any no to push in queue ")))
+p.insert(0,int(input("enter any no to push in queue ")))
+print('after push a element the queue is',p)
+del p[0]								 #for removing a element from list(queue)
+print("after deleting the queue is",p)
+
+
+print("question no 7")
+v=0
+n=0
+q=[4,5,6,7,8,9,10,2,12,15]
+print("list is",q)
+for a in range(0,10):
+	if q[a]%2==0:			# checking even no
+		v=v+1 
+	if q[a]%2!=0:			#checking odd no
+		n=n+1
+print("the odd no is in list=",n) 
+print("the even no is in list=",v)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
