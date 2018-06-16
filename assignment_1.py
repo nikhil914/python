4968000f14d037c27d1f4734a2be0f6dda02ef98
rahul6316057 <rt56277@gmail.com>
2018-06-16 13:33:40 +0530

    assignments are updated

---
+++ assignment_1.py
@@ -0,0 +1,47 @@
+print("question 1")
+print("hi")
+print("hello world")
+print("\n\n\n")
+
+
+print("question 2")
+x="Acad"
+y="View"
+z=x+y
+print(z)
+print("\n\n\n")
+
+
+print("question 3")
+a=int(input("enter any number "))
+b=float(input("enter any decimal number "))
+c=input("enter any string ")
+print(a,b,c)
+print("\n\n\n")
+
+print("question 4")
+print('let\'s get started')
+print("\n\n\n")
+
+
+print("question 5")
+p="rahul kumar"
+course="python"
+fees=5000
+print("%s,%s,%d"%(p,course,fees))
+print("\n\n\n")
+
+print("question 6")
+name="tony stark"
+salary=10000000
+print("%s,%d"%(name,salary))
+
+
+
+
+
+
+
+
+
+
