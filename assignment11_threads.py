e0667b7a67749c86e351fa86c3d5cc6333b166f9
rahul6316057 <rt56277@gmail.com>
2018-06-22 12:28:18 +0530

    assignment 11 done

---
+++ assignment11_threads.py
@@ -0,0 +1,47 @@
+import threading
+from threading import Thread
+import time
+
+print("question no 01")
+def display():
+    time.sleep(5)
+    print("hello !")
+t=Thread(target=display)
+t.start()
+
+
+print("question no 02")
+def table():
+    for x in range (1,11):
+        t.join()
+        print(x)
+        time.sleep(1)
+t1=Thread(target=table)
+t1.start()
+
+
+
+print("question no 03")
+def list_element_print():
+    m=2
+    for x in l:
+        t1.join()
+        print(x)
+        time.sleep(m)
+        m=m+2
+l=[5,8,9,0,3]
+t2=Thread(target=list_element_print)
+t2.start()
+
+
+
+print("question no 04")
+def factorial():
+    t2.join()
+    x=int(input("enter any no to find factorial:"))
+    fact=1
+    for a in range(1,x+1):
+        fact=fact*a
+    print(fact)
+t3=Thread(target=factorial)
+t3.start()
